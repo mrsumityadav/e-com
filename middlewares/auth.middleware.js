341942da1211f459279bca5b47464272e4b55205
Sumit Yadav <sumitsingh910591@gmail.com>
2026-01-22 16:58:19 +0530

    change authenticate

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,30 +4,33 @@ const jwt = require('jsonwebtoken')
 
 module.exports.isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
+        const authHeader = req.headers.authorization
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "Authorization header missing" })
+        }
+
+        const token = authHeader.split(" ")[1]
 
         const isTokenBlockListed = await blockListedModel.findOne({ token })
         if (isTokenBlockListed) {
-            return res.status(401).json({
-                message: 'Unauthorized'
-            })
+            return res.status(401).json({ message: "Token revoked" })
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await userModel.findOne({ _id: decoded._id })
 
+        const user = await userModel.findById(decoded._id)
         if (!user) {
-            return res.status(401).json({
-                message: 'Unauthorized'
-            })
+            return res.status(401).json({ message: "User not found" })
         }
 
         req.user = user
         next()
     } catch (err) {
-        next(err)
+        return res.status(401).json({ message: "Invalid or expired token" })
     }
 }
+
 
 module.exports.isSeller = async (req, res, next) => {
     try {
